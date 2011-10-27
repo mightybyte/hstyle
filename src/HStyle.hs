9e34bc8e379db3fd23b9772ee7ed0187435a39d9
Jasper Van der Jeugt <m@jaspervdj.be>
2011-10-27 23:31:59 +0200

    Propagate Block through the codebase

--- src/HStyle.hs
+++ src/HStyle.hs
@@ -7,30 +7,30 @@ module HStyle
 import Control.Monad (forM_, unless)
 import System.Environment (getArgs)
 
-import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Language.Haskell.Exts.Annotated as H
 
 import HStyle.Alignment
+import HStyle.Block
 import HStyle.Types
 
-takeLines :: H.SrcSpanInfo -> Text -> [Text]
-takeLines ssi = take (end - start + 1) . drop (start - 1) . T.lines
+subBlock' :: H.SrcSpanInfo -> Block -> Block
+subBlock' ssi = subBlock start end
   where
     span' = H.srcInfoSpan ssi
     start = H.srcSpanStartLine span'
     end   = H.srcSpanEndLine span'
 
-runRule :: Text -> H.Module H.SrcSpanInfo -> Rule -> IO ()
-runRule text md (selector, check) = forM_ (selector md) $ \ssi -> do
-    let lines'   = takeLines ssi text
-        problems = check lines'
+runRule :: Block -> H.Module H.SrcSpanInfo -> Rule -> IO ()
+runRule block md (selector, check) = forM_ (selector md) $ \ssi -> do
+    let block'   = subBlock' ssi block
+        problems = check block'
     unless (null problems) $ do
         T.putStrLn $ T.replicate 78 "-"
-        T.putStr   $ T.unlines lines'
+        T.putStr   $ prettyBlock block'
         T.putStrLn $ T.replicate 78 "-"
-    forM_ (check $ takeLines ssi text) $ \(i, problem) -> do
+    forM_ problems $ \(i, problem) -> do
         let line = i + H.srcSpanStartLine (H.srcInfoSpan ssi)
         T.putStrLn $ T.pack (show line) `T.append` ": " `T.append` problem
 
@@ -39,17 +39,17 @@ typeSigSelector (H.Module _ _ _ _ decls) = [ssi | H.TypeSig ssi _ _ <- decls]
 typeSigSelector _                        = []
 
 typeSigCheck :: Check
-typeSigCheck lines' = case checkAlignment alignment of
+typeSigCheck block = case checkAlignment alignment of
     Just _  -> [(0, "Bad alignment")]
     Nothing -> []
   where
-    alignment = alignmentOf ["::", "=>", "->"] lines'
+    alignment = alignmentOf ["::", "=>", "->"] $ toLines block
 
 main :: IO ()
 main = do
     [file] <- getArgs
     contents <- readFile file
-    let text = T.pack contents
+    let block = fromText $ T.pack contents
     case H.parseModule contents of
-        H.ParseOk x -> runRule text x (typeSigSelector, typeSigCheck)
+        H.ParseOk x -> runRule block x (typeSigSelector, typeSigCheck)
         err         -> putStrLn $ show err
--- src/HStyle/Types.hs
+++ src/HStyle/Types.hs
@@ -7,12 +7,14 @@ module HStyle.Types
 import Data.Text (Text)
 import Language.Haskell.Exts.Annotated (Module, SrcSpanInfo)
 
+import HStyle.Block
+
 -- | Selects a portion from a haskell module
 type Selector = Module SrcSpanInfo -> [SrcSpanInfo]
 
 -- | Takes a number of lines, and notifies of problems on each line. Indices in
 -- the result are zero-based.
-type Check = [Text] -> [(Int, Text)]
+type Check = Block -> [(Int, Text)]
 
 -- | A selector and a check...
 type Rule = (Selector, Check)
